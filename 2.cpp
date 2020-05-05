4ea473b780e2bf0f470875f4cbf0a373da4d77d1
Caio Eduan Mazzolli <62838762+caiomazz@users.noreply.github.com>
2020-05-05 16:57:11 -0300

    Algoritmo para calcular a metade de um valor

--- 2.cpp
+++ 2.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
+#include <locale.h>
 
 using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "Portuguese");
+    
     float numero, metade;
     cout<<"informe um valor: ";
     cin>> numero;
 
     metade = numero / 2.0;
 
-    cout<<"A metade deste valor é de: "<<metade;
+    cout<<"A metade deste valor Ã© de: "<<metade;
     return 0;
 }
